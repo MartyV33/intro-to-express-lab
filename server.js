e3b1f15b4fd69949b4ff5e576b4d23b53a776cc7
MartyV33 <martinverdoza33@gmail.com>
2024-08-26 20:37:09 -0500

    Added route for rolling dice

--- server.js
+++ server.js
@@ -9,3 +9,16 @@ const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });  // This is to listen on port 3000
+
+app.get('/roll/:number', (req, res) => {
+    const number = parseInt(req.params.number, 10);
+    // We are defining a route for rolling the dice
+
+    if (isNaN(number)) {
+        res.send('You must specify a number.');
+    } else {
+        const randomNumber = Math.random() * (number + 1);
+        res.send(`You rolled a  ${randomNumber}.`);
+    }
+});
+
