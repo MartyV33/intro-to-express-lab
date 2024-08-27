dfcc97d670c311daa86f8b64f9428c767687b963
MartyV33 <martinverdoza33@gmail.com>
2024-08-26 20:19:14 -0500

    Defining our route for greeting the user and listening on port 3000

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express');
 const app = express(); 
+app.get('/greetings/:username',(req, res) => {
+    const username = req.params.username;
+    res.send(`Well hello there, ${username}!`);
+});  // We're defining a route for greeting the user
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});  // This is to listen on port 3000
